2e433a367e2bc12ce440cf1e3461d8439616ca2a
Danielle Zephyr Malament <danielle.malament@gmail.com>
2018-12-26 17:01:03 -0500

    license block to meta.sh

--- meta.sh
+++ meta.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright 2018 Danielle Zephyr Malament
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 _python_venv_prompt () {
     # Note: 'pyenv activate' uses $PYENV_VERSION, which pyenv checks first.
     # In order for a .python_version to take effect (which uses
